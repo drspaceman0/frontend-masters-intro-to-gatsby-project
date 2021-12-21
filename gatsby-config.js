aba846716ee9a2d13dbdc048d690b6bcae221d41
Eric Marsh <ericmrsh9@gmail.com>
2021-12-20 16:34:34 -0800

    Configure Gatsby with MDX

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,33 @@
 module.exports = {
     siteMetadata: {
-      siteUrl: "https://www.yourdomain.tld",
-      title: "Frontend Masters Intro to Gatsby",
-      description: "Frontend Masters Intro to Gatsby course project",
+      siteUrl: 'https://www.yourdomain.tld',
+      title: 'Frontend Masters Intro to Gatsby',
+      description: 'Frontend Masters Intro to Gatsby course project',
       image:
-        "https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg",
+        'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
     },
-    plugins: ['gatsby-plugin-react-helmet'],
+    plugins: [
+      'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-page-creator',
+      options: {
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          posts: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+    ],
   };
