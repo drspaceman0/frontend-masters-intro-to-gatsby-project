1ab4f1f047e8d29167e0ba2a2a7dc32f13b97ef1
Eric Marsh <ericmrsh9@gmail.com>
2021-12-20 16:18:00 -0800

    Add Layout Component and Apply To Pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,12 @@
-import * as React from 'react'; 
-import { Seo } from '../components/seo.js';
+import * as React from 'react';  
+import { Link } from 'gatsby';
+import Layout from '../components/layout.js';
 
-import { Link, useStaticQuery, graphql } from 'gatsby';
  export default function IndexPage() {
-  const data = useStaticQuery(graphql`
-    query GetSiteTitle {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-  const meta = data?.site?.siteMetadata ?? {};
    return (
-    <>
-    <Seo />
-      <header>
-        <Link to="/">{meta.title}</Link>
-      </header>
-       <main>
-         <h1>Hello Frontend Masters!</h1>
-         <Link to="/about">About this site</Link>
-       </main>
-    </>
+     <Layout>
+        <h1>Hello Frontend Masters!</h1>
+        <Link to="/about">About this site</Link>
+     </Layout>
    );
  }
