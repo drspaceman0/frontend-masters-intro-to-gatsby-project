375deb29e85304746dbd5527f0ec6b706855dcf5
Eric Marsh <ericmrsh9@gmail.com>
2021-12-20 16:02:10 -0800

    Add Query To Grab Static Metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,25 @@
-import * as React from "react";
-import { Link } from "gatsby";
-
-export default function IndexPage() {
-  return (
-    <main>
-      <h1>Hello Frontend Masters!</h1>
-      <Link to="/about">About this site</Link>
-    </main>
-  );
-}
+import * as React from 'react'; 
+import { Link, useStaticQuery, graphql } from 'gatsby';
+ export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query GetSiteTitle {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+  const meta = data?.site?.siteMetadata ?? {};
+   return (
+    <>
+      <header>
+        <Link to="/">{meta.title}</Link>
+      </header>
+       <main>
+         <h1>Hello Frontend Masters!</h1>
+         <Link to="/about">About this site</Link>
+       </main>
+    </>
+   );
+ }
